65c52a5f6f655fbdb86ac2d19617935a9bca3eda
Kendall Miller <kendall@gibraltarsoftware.com>
2014-11-28 18:51:48 -0500

    Updated licensing to correctly reflect Apache license

--- src/Agent.PostSharp.Test/TimeSpanCollection.cs
+++ src/Agent.PostSharp.Test/TimeSpanCollection.cs
@@ -1,22 +1,22 @@
-﻿#region Released to Public Domain by eSymmetrix, Inc.
-
-/********************************************************************************
- *   This file is sample code demonstrating Gibraltar integration with PostSharp
- *   
- *   This sample is free software: you have an unlimited rights to
- *   redistribute it and/or modify it.
- *   
- *   This sample is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *   
- *******************************************************************************/
+﻿// /*
+//    Copyright 2013 Gibraltar Software, Inc.
+//    
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// */
 
 using System;
 using System.Collections.Generic;
 using Gibraltar.Agent.Metrics;
-
-#endregion
 
 namespace GSharp.Samples
 {
